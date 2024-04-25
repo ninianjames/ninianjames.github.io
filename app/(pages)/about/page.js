e5fefad9eb6c7cae8404ccd024ff3ad01f0237c5
Ninian <101335198+ninianjames@users.noreply.github.com>
2024-04-25 11:46:08 +0100

    Update page.js

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -6,7 +6,7 @@ const page = () => {
       <h1 className="text-colour-headings text-3xl md:text-5xl font-bold">About me</h1>
       <div className="mt-10 grid grid-cols-1 md:grid-cols-2">
         <div className="col-span-1">
-          <p className="mb-3">I am a third year Criminology PhD student at Swansea University.</p>
+          <p className="mb-3">I am a final year Criminology PhD student at Swansea University.</p>
           <p className="my-3">I study gender in the online extreme right, and I do this by scraping and crawling extremist websites and analysing the textual content I find there.</p>
           <p className="my-3">My background is in Criminology, Psychology, and social research methods.</p>
           <p className="my-3">As part of my PhD research, I produce maps of these websites, some of which can be seen on the <a href="/graphs" className="underline decoration-dotted hover:text-colour-link">Graphs</a> page of this website.</p>
